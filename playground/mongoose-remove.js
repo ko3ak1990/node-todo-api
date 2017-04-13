b52ba9e01e039add527c220af9d8f73c6dc6c51b
Ievgen Umanets <ievgen@umanets.gmail.com>
2017-04-13 16:15:43 +0300

    DELETE todo api method added

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,7 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+const {ObjectID} = require('mongodb');
+
+
+var id = '58ee38bde45a6c634f43efdc11';
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,7 +49,29 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({
+            status: 200,
+            deleted: todo
+        });
+        console.log(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
+
+app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
 
