dd1a3947690269444bdc021f5b8da8c1a9bc3f3a
Ievgen Umanets <ievgen@umanets.gmail.com>
2017-04-13 11:49:02 +0300

    add test server.js

--- server/server.js
+++ server/server.js
@@ -1,85 +1,56 @@
-const {ObjectID} = require('mongodb');
-
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-app.use(bodyParser.json());
-app.post('/todos', (req, res) => {
+const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 
     todo.save().then((doc) => {
-        return res.status(200).send(doc);
+        res.send(doc);
     }, (e) => {
-        console.log(e);
-        return res
-            .status(400).send(e);
+        res.status(400).send(e);
     });
-
-    console.log(req.body);
-
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({
-            msg: "succesfully found objects",
-            todoArr: todos
-        }).status(200);
+        res.send({todos});
     }, (e) => {
-        console.log(e);
-        res.send({
-            msg: "FAILED",
-            status: 400
-        }).status(400);
+        res.status(400).send(e);
     });
 });
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if (ObjectID.isValid(id)) {
-        Todo.findById(id).then((todo) => {
-                if (todo) {
-                    res.send({
-                        msg: "succesfully found object",
-                        param: id,
-                        todo: todo
-                    }).status(200);
-                } else {
-                    res.status(404).send({
-                        msg: "object not found",
-                        param: id,
-                        status: 404,
-                        todo: todo
-                    });
-                }
-            }
-            , (e) => {
-                console.log(e);
-                res.status(400).send({
-                    msg: "BAD REQUEST",
-                    status: 400
-                });
-            });
-    } else {
-        res.send({
-            msg: "Invalid request",
-            status: 404
-        }).status(404);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
     }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
-
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
