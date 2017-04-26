8e72fb9b7b5ab5a0319447a678f2b8c360ff8ec7
Ievgen Umanets <ievgen@umanets.gmail.com>
2017-04-26 09:58:54 +0300

    middleware updated set headers

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(function (req, response, next) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, X-Auth,Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
     next();
 });
